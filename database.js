209aac9e7a5ca0bf87b32c27efd14c11dcfe9975
Ludvig <91793530+TheLudde235@users.noreply.github.com>
2023-02-09 19:03:42 +0100

    created database.js

---
+++ database.js
@@ -0,0 +1,46 @@
+import fs from 'fs';
+
+export default class jsonDB {
+  #jsonData = '';
+  #path; 
+  constructor(path = './database.json') {
+    this.#path = path;
+  }
+  #updateData() {
+    this.#jsonData = fs.readFileSync(this.#path);
+  }
+  async get(key) {
+    this.#updateData();
+    return JSON.parse(this.#jsonData)[key];
+  }
+  async list(prefix) {
+    this.#updateData();
+    const jsObject = JSON.parse(this.#jsonData);
+    const out = [];
+    for (const child in jsObject) {
+      const split = child.split(prefix);
+       if (!prefix || (split.length > 0 && split[0].length <= 0)) {
+        out.push(child);
+      }
+    }
+    return out;
+  }
+  async set(key, value) {
+    this.#updateData();
+    const jsObject = JSON.parse(this.#jsonData);
+    jsObject[key] = value;
+    fs.writeFileSync(this.#path, JSON.stringify(jsObject));
+    return jsObject;
+  }
+  async delete(key) {
+    this.#updateData();
+    const jsObject = JSON.parse(this.#jsonData);
+    const out = {};
+    for (const child in jsObject) {
+      if (child === key) continue;
+      out[child] = jsObject[child];
+    }
+    fs.writeFileSync(this.#path, JSON.stringify(out));
+    return out;
+  }
+}
